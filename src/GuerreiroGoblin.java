5d63bd4600b496642eab2f1d399f17f79b534f4b
Rafael Leal <rafaellealde2001@gmail.com>
2024-06-25 05:54:04 -0300

    adicionado giblins

---
+++ src/GuerreiroGoblin.java
@@ -0,0 +1,56 @@
+import java.io.Serializable;
+
+class Guerreiro extends Heroi implements Serializable {
+
+    //atributos
+    private int forca;
+
+    //construtores
+    public Guerreiro(String nome, String raca) {
+        super(nome, raca);
+        inicializarAtributos();
+    }
+
+    //modulos
+    @Override
+    public void inicializarAtributos() {
+        this.nome = "Goblin Guerreiro";
+        this.raca = "Goblin";
+        this.hp = 50;
+        this.ca = 18;
+        this.iniciativa = 2;
+        this.forca = 3;
+    }
+
+    //acoes
+    @Override
+    public int rolarIniciativa() {
+        return rolarD20() + iniciativa;
+    }
+
+    @Override
+    public int atacar() {
+        int hit = rolarD20() + forca;
+        int dmg = rolarD10() + forca;
+        return dmg;
+    }
+
+    @Override
+    public int ataqueEspecial() {
+        int hit = (rolarD8() * 2) + forca;
+        int dmg = (rolarD8() * 2) + forca;
+        return dmg;
+    }
+
+    //getter
+    public int getForca() {
+        return forca;
+    }
+
+    //to string
+    @Override
+    public String toString() {
+        String txt = "Guerreiro" + "\n" + nome + "\n(" + raca + ")\n";
+        return txt;
+    }
+}
---
+++ src/MagoGoblin.java
@@ -0,0 +1,56 @@
+import java.io.Serializable;
+
+class Mago extends Heroi implements Serializable {
+
+    //atributos
+    private int magia;
+
+    //construtores
+    public Mago(String nome, String raca) {
+        super(nome, raca);
+        inicializarAtributos();
+    }
+
+    //modulos
+    @Override
+    public void inicializarAtributos() {
+        this.nome = "Goblin Mago";
+        this.raca = "Goblin";
+        this.hp = 25;
+        this.ca = 13;
+        this.iniciativa = 1;
+        this.magia = 2;
+    }
+
+    //acoes
+    @Override
+    public int rolarIniciativa() {
+        return rolarD20() + iniciativa;
+    }
+
+    @Override
+    public int atacar() {
+        int hit = rolarD20() + magia;
+        int dmg = rolarD8() + magia;
+        return dmg;
+    }
+
+    @Override
+    public int ataqueEspecial() {
+        int hit = rolarD10() + magia;
+        int dmg = (rolarD10() * 3) + magia;
+        return dmg;
+    }
+
+    //getter
+    public int getmagia() {
+        return magia;
+    }
+
+    //to string
+    @Override
+    public String toString() {
+        String txt = "Mago" + "\n" + nome + "\n(" + raca + ")\n";
+        return txt;
+    }
+}
